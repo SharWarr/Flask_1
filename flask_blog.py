43fdadd90e8845c5817c180c5d99f7601a6ae831
SharWarr <sharanyajwarrier@gmail.com>
2021-03-14 18:47:21 +0530

    first commit

---
+++ flask_blog.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+posts = [
+    {
+        'author':'Jane Doe',
+         'title':'Blog post',
+         'content':'In the 2010s, the majority are interactive Web 2.0 websites, allowing visitors to leave online comments, and it is this interactivity that distinguishes them from other static websites. In that sense, blogging can be seen as a form of social networking service. Indeed, bloggers not only produce content to post on their blogs but also often build social relations with their readers and other bloggers.',
+         'date_posted':'April 20 2021'
+    },
+    {
+        'author':'Jane Warrier',
+         'title':'MY blog post ',
+         'content':'The emergence and growth of blogs in the late 1990s coincided with the advent of web publishing tools that facilitated the posting of content by non-technical users who did not have much experience with HTML or computer programming. Previously, a knowledge of such technologies as HTML and File Transfer Protocol had been required to publish content on the Web, and early Web users therefore tended to be hackers and computer enthusiasts.',
+         'date_posted':'April 22 2021'
+    }
+]
+
+@app.route("/")
+def hello():
+    return render_template('home.html',posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html',title='About')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
