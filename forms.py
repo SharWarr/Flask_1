df9571608d95e579779905cd8bb5f0df4c4dead8
SharWarr <sharanyajwarrier@gmail.com>
2021-03-14 20:06:05 +0530

    commit 2

--- forms.py
+++ forms.py
@@ -1,2 +1,6 @@
 from flask import Flask, render_template, flash, request
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+
+class RegistrationForm(Flask):
+    username = StringField('useraname',validators=[])
+
